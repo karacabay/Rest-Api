3758aef4ecae1a798a4d93725c9bbc980f1844f9
eneskaracabay <deveneskaracabay@gmail.com>
2023-08-14 20:21:13 +0300

    port update

--- test/add_new_card.py
+++ test/add_new_card.py
@@ -14,14 +14,16 @@ def add_new_card(user_no, auth_code, card_number, endpoint, is_selected=True):
         'IsSelected': is_selected
     }
     json_data = json.dumps(data)
-    return requests.post(url+endpoint, headers=headers, json=json_data)
+    u = url+endpoint
+
+    return requests.post(u, headers=headers, json=json_data)
 
 
 with open('../config.json', 'r') as fp:
     server_cfg = json.load(fp)['ServerConfig']
 
 url = f"http://{server_cfg['Host']}:{server_cfg['Port']}"
-# url = 'http://ec2-13-48-149-251.eu-north-1.compute.amazonaws.com:80'
+url = 'http://loadbalancer-1498206481.eu-north-1.elb.amazonaws.com'
 user_no = 2250058
 auth_code = 'qUiiNr3uzcd'
 card_number = ''.join([str(random.randint(0, 9)) for _ in range(16)])
